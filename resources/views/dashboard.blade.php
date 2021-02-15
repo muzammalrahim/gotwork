7de45f2212def8a5a49af1562a79a3134e576094
Zeeshan Niaz <zeeshanniaz736@gmail.com>
2021-02-15 19:46:27 +0500

    Email Verification Completed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,10 @@
+<?php
+$verified = false;
+if (Auth::user()->email_verified_at) {
+    $verified = true;
+}
+?>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -5,13 +12,21 @@
         </h2>
     </x-slot>
 
+    
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                @if($verified)
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
                 </div>
+                @else
+                <div class="p-6 bg-red-400 border-b border-gray-200">
+                    Please Verify Email First!
+                </div>
+                @endif
             </div>
         </div>
     </div>
+    
 </x-app-layout>
